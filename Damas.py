adbc37a63452093d9a97290aeaba4db2aab19e18
Juan <qwaser34@outlook.com>
2020-03-02 15:54:35 -0400

    agregando comer normales y movimiento de reinas

--- Damas.py
+++ Damas.py
@@ -31,7 +31,7 @@ class DamasChinas:
 
 class PiesesWhiteBlack(DamasChinas):
     turn = False
-    def movement(self, Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,turn):
+    def movement_normal_pieses(self, Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,turn):
         self.Row_Postion_Current = Row_Postion_Current
         self.Column_Postion_current = Column_Postion_current
         self.Row_Postion_Late = Row_Postion_Late
@@ -60,6 +60,29 @@ class PiesesWhiteBlack(DamasChinas):
                                 #change to queen
             if(self.Pieses_Black == self.Board[1][Column_Postion_Late]):
                 self.Board[1][Column_Postion_Late] = self.Pieses_Queen_Black
+    
+    def movement_queen_pieses(self, Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,turn):
+        self.Row_Postion_Current = Row_Postion_Current
+        self.Column_Postion_current = Column_Postion_current
+        self.Row_Postion_Late = Row_Postion_Late
+        self.Column_Postion_Late = Column_Postion_Late
+
+        if(turn):#turn Queen white
+            if(self.Row_Postion_Late%2 != 0 and self.Column_Postion_Late%2 == 0 or self.Row_Postion_Late%2 == 0 and self.Column_Postion_Late%2 != 0):
+                self.Board[Row_Postion_Current][Column_Postion_current] = self.Pieses_Queen_White
+                self.Board[Row_Postion_Late][Column_Postion_Late] = '⬜'
+            else:
+                self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
+                self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_Queen_White
+
+        else: #turn Queen black
+            if(self.Row_Postion_Late%2 != 0 and self.Column_Postion_Late%2 == 0 or self.Row_Postion_Late%2 == 0 and self.Column_Postion_Late%2 != 0):
+                self.Board[Row_Postion_Current][Column_Postion_current] = self.Pieses_Queen_Black
+                self.Board[Row_Postion_Late][Column_Postion_Late] = '⬜'
+            else:    
+                self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
+                self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_Queen_Black
+
 
     def detect(self):# encontrar piesa
         pieseswhite = [self.Pieses_White,self.Pieses_Queen_White]
@@ -98,19 +121,12 @@ class PiesesWhiteBlack(DamasChinas):
             elif(Player not in Players):
                 print("Error, Does not exist")
 
-    # def verification(self,i):
-    #     if(self.players == True):
-    #         i = 0
-    #         return i
-    #     else:
-    #         i = 1
-    #         return i
-        # return self.players? 0 : 1;
+
 
     def movement_pieses(self):
         letter_to_number = {
             "A": 1,"a": 1,"B": 2,"b": 2,"C": 3,"c": 3,"D": 4,"d": 4,"E": 5,"e": 5,"F": 6,"f": 6,"G": 7,"g": 7,"H": 8,"h": 8,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8}
-        i= self.players() if 0 else 1
+        # i= self.players() if 0 else 1
 
         while(True):
             
@@ -125,54 +141,62 @@ class PiesesWhiteBlack(DamasChinas):
             Column_Postion2 = Row_Late[1]
             Column_Postion_Late = letter_to_number[Column_Postion2]
 
-            Value_Row_White = Row_Postion_Late - Row_Postion_Current
-            Value_Col_White = Column_Postion_Late - Column_Postion_current
-            Value_Row_Black = Row_Postion_Current - Row_Postion_Late
-            Value_Col_Black = Column_Postion_current - Column_Postion_Late
+            if(self.Board[Row_Postion_Current][Column_Postion_current] == self.Pieses_White):
 
-            if(self.Board[Row_Postion_Current][Column_Postion_current] == self.Pieses_White and abs(Value_Row_White) == 1):
-                    if(self.Board[Row_Postion_Late][Column_Postion_Late] == self.Pieses_Black):
-                        self.Board[Row_Postion_Late][Column_Postion_Late] = '⬛'
-                        if(Value_Row_White >= 0):
-                            Row_Postion_Late=Row_Postion_Late+1 #fila de comer
-                            Column_Postion_Late=Column_Postion_Late-1 #columna de comer
-                        else:
-                            Row_Postion_Late=Row_Postion_Late-1
-                            Column_Postion_Late=Column_Postion_Late+1
-                        self.movement(Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,PiesesWhiteBlack.turn)
-                        os.system("cls")
-                        self.presentation()
-                        PiesesWhiteBlack.turn = False
-                        print("shift change, Black")
-                        # r = r + 1
-                        # self.verification(r) #aqui iba el i+=1
-                    else:
-                        self.movement(Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,PiesesWhiteBlack.turn)
-                        os.system("cls")
-                        self.presentation()
-                        PiesesWhiteBlack.turn = False
-                        print("shift change, Black")
-                        # r = r + 1
-                        # self.verification(r) #aqui iba el i+=1
+                    # de izquierda arriba a derecha abajo 
+                if(self.Board[Row_Postion_Late - 1][Column_Postion_Late - 1] == self.Pieses_Black):
+                    self.Board[Row_Postion_Late - 1][Column_Postion_Late - 1] = '⬛'
+                    self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_White
+                    self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
+                
+                    # de derecha arriba a izquierda abajo
+                if(self.Board[Row_Postion_Late - 1][Column_Postion_Late + 1] == self.Pieses_Black):
+                    self.Board[Row_Postion_Late - 1][Column_Postion_Late + 1] = '⬛'
+                    self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_White
+                    self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
 
-            else:
-                if(self.Board[Row_Postion_Current][Column_Postion_current] == self.Pieses_Black and PiesesWhiteBlack.turn == False and abs(Value_Row_Black) == 1 and Value_Col_Black <= 1):
-                    if(self.Board[Row_Postion_Late][Column_Postion_Late] == self.Pieses_White):
-                        self.Board[Row_Postion_Late][Column_Postion_Late] = '⬛'
-                        if(Value_Row_Black <= 0):
-                            Row_Postion_Late=Row_Postion_Late+1 # fila de comer
-                            Column_Postion_Late=Column_Postion_Late+1 #columna de comer
-                        else:
-                            Row_Postion_Late=Row_Postion_Late-1
-                            Column_Postion_Late=Column_Postion_Late-1
-                    self.movement(Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,PiesesWhiteBlack.turn)
                     os.system("cls")
                     self.presentation()
-                    PiesesWhiteBlack.turn = True
-                    print("shift change, White")
-                    # i += 1
+                    PiesesWhiteBlack.turn = False
+                    print("shift change, Black")
+
+                else:
+                    self.movement_normal_pieses(Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,PiesesWhiteBlack.turn)
+                    os.system("cls")
+                    self.presentation()
+                    PiesesWhiteBlack.turn = False
+                    print("shift change, Black")
+
+            else:
+                if(self.Board[Row_Postion_Current][Column_Postion_current] == self.Pieses_Black and PiesesWhiteBlack.turn == False):
+
+                        # de derecha abajo a izquierda arriba
+                    if(self.Board[Row_Postion_Late + 1][Column_Postion_Late + 1] == self.Pieses_White):
+                        self.Board[Row_Postion_Late + 1][Column_Postion_Late + 1] = '⬛'
+                        self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_Black
+                        self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
+                    
+                        # de izquierda abajo a derecha arriba
+                    if(self.Board[Row_Postion_Late + 1][Column_Postion_Late - 1] == self.Pieses_White):
+                        self.Board[Row_Postion_Late + 1][Column_Postion_Late - 1] = '⬛'
+                        self.Board[Row_Postion_Late][Column_Postion_Late] = self.Pieses_Black
+                        self.Board[Row_Postion_Current][Column_Postion_current] = '⬛'
+
+                        os.system("cls")
+                        self.presentation()
+                        PiesesWhiteBlack.turn = True
+                        print("shift change, White")
+
+                    else:
+                        self.movement_normal_pieses(Row_Postion_Current,Column_Postion_current,Row_Postion_Late,Column_Postion_Late,PiesesWhiteBlack.turn)
+                        os.system("cls")
+                        self.presentation()
+                        PiesesWhiteBlack.turn = True
+                        print("shift change, White")
+                        
                 else:
                     print("Invalid Movement, try again")
+            
 
     def presentation(self):
         super().state()
